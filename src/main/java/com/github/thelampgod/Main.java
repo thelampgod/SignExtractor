b26018a2f8c2eb714704b26be6d0947de954d6b4
lamp <virtual@riseup.net>
2024-08-23 18:16:33 +0200

    Initial commit

---
+++ src/main/java/com/github/thelampgod/Main.java
@@ -0,0 +1,54 @@
+package com.github.thelampgod;
+
+import net.querz.mca.MCAFile;
+import net.querz.nbt.CompoundTag;
+import net.querz.nbt.io.snbt.SNBTWriter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+
+    public static void main(String... args) throws IOException {
+        if (args == null || args.length == 0) {
+            System.err.println("specify region folder");
+            System.exit(1);
+        }
+        String regionFolder = args[0];
+        String output = ".";
+        if (args.length == 2) {
+            output = args[1];
+        }
+        FileWriter writer = new FileWriter(output + "/signs.txt");
+        AtomicInteger chunks = new AtomicInteger();
+        Arrays.asList(new File(regionFolder).listFiles()).stream()
+                .filter(file -> file.getName().endsWith(".mca"))
+                .parallel()
+                .forEach(file -> {
+                    MCAFile mca = new MCAFile(file);
+                    try {
+                        mca.load();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println("Processing " + mca.getName());
+                    final SNBTWriter nbt = new SNBTWriter();
+                    mca.forEach(chunk -> {
+                        chunk.getData().getList("block_entities").stream().map(tag -> (CompoundTag) tag).forEach(tile -> {
+                            if (!tile.getString("id").equals("minecraft:sign")) return;
+                            try {
+                                writer.write(nbt.toString(tile) + "\n");
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        });
+                        chunks.incrementAndGet();
+                    });
+                });
+        System.out.printf("Processed %s chunks", chunks);
+        writer.close();
+    }
+}
