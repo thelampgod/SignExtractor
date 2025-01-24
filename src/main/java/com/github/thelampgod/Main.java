3df41bb4022c415dbdb021c0c284a06ce8db0a59
lamp <virtual@riseup.net>
2025-01-24 11:39:21 +0100

    Fix some formatting

--- src/main/java/com/github/thelampgod/Main.java
+++ src/main/java/com/github/thelampgod/Main.java
@@ -22,51 +22,53 @@ public class Main {
         if (args.length == 2) {
             output = args[1];
         }
+
         try (FileWriter writer = new FileWriter(output + "/signs.txt")) {
             writer.write("x,y,z,line1,line2,line3,line4,color,glowing\n");
             AtomicInteger chunks = new AtomicInteger();
             Arrays.asList(new File(regionFolder).listFiles()).stream()
                     .filter(file -> file.getName().endsWith(".mca"))
                     .parallel()
-                    .forEach(file
-                            -> {
+                    .forEach(file -> {
                         MCAFile mca = new MCAFile(file);
                         try {
                             mca.load();
                         } catch (IOException e) {
-                            throw new RuntimeException(e);
+                            return;
                         }
 
                         System.out.println("Processing " + mca.getName());
-                        mca.forEach(chunk
-                                -> {
-                            if (chunk != null && chunk.getData() != null) {
-                                chunk.getData().getList("block_entities").stream().map(tag -> (CompoundTag) tag).forEach(tile
-                                        -> {
-                                    if (!tile.getString("id").equals("minecraft:sign")) {
-                                        return;
-                                    }
-                                    CompoundTag front_text = tile.getCompound("front_text");
-                                    ListTag messages = front_text.getList("messages");
-                                    String formatted = String.format("%d,%d,%d,%s,%s,%s,%s,%s,%s",
-                                            tile.getInt("x"),
-                                            tile.getInt("y"),
-                                            tile.getInt("z"),
-                                            messages.getString(0),
-                                            messages.getString(1),
-                                            messages.getString(2),
-                                            messages.getString(3),
-                                            front_text.getString("color"),
-                                            front_text.getBoolean("has_glowing_text")
-                                    );
-                                    try {
-                                        writer.write(formatted + "\n");
-                                    } catch (IOException e) {
-                                        throw new RuntimeException(e);
-                                    }
-                                });
-                                chunks.incrementAndGet();
-                            }
+                        mca.forEach(chunk -> {
+                            if (chunk == null || chunk.getData() == null) return;
+
+                            chunk.getData().getList("block_entities").stream()
+                                    .map(tag -> (CompoundTag) tag)
+                                    .forEach(tile -> {
+                                        if (!tile.getString("id").equals("minecraft:sign")) {
+                                            return;
+                                        }
+
+                                        CompoundTag front_text = tile.getCompound("front_text");
+                                        ListTag messages = front_text.getList("messages");
+                                        String formatted = String.format("%d,%d,%d,%s,%s,%s,%s,%s,%s",
+                                                tile.getInt("x"),
+                                                tile.getInt("y"),
+                                                tile.getInt("z"),
+                                                messages.getString(0),
+                                                messages.getString(1),
+                                                messages.getString(2),
+                                                messages.getString(3),
+                                                front_text.getString("color"),
+                                                front_text.getBoolean("has_glowing_text")
+                                        );
+
+                                        try {
+                                            writer.write(formatted + "\n");
+                                        } catch (IOException e) {
+                                            throw new RuntimeException(e);
+                                        }
+                                    });
+                            chunks.incrementAndGet();
                         });
                     });
             System.out.printf("Processed %s chunks", chunks);
